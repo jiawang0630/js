70a6bff1e53e7a4854440bd6c86f684bf416a6a6
jiawang0630 <wscjack0630@gmail.com>
2022-01-18 17:04:48 +0800

    v3-1

---
+++ index3-1.js
@@ -0,0 +1,15 @@
+let score = 75;
+/*
+if(score >= 60){
+    console.log("及格");
+}else{
+    console.log("不及格");
+}
+*/
+if(score >= 90){
+    console.log("A級");
+}else if(score >= 80){
+    console.log("B級");
+}else if(score >= 70){
+    console.log("C級");
+}
