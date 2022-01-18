88d0f2b61cca6fb594378c4bbd929a261d7687b8
jiawang0630 <wscjack0630@gmail.com>
2022-01-18 11:37:06 +0800

    v2-4

---
+++ index2-4.js
@@ -0,0 +1,12 @@
+let myName = "++++";
+console.log(myName);
+myName = "123";
+console.log(myName);
+
+const PI = 3.14159;
+console.log(PI);
+
+var age = 25;
+console.log(age);
+var age = 40;
+console.log(age);
